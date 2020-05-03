baee15dfe5df1492927d99d6b55de996665c7f72
Savva Kodeikin <savva.kodeikin@gmail.com>
2020-05-03 16:23:09 +0200

    Gave the project a more abstract name

--- django_ecom/wsgi.py
+++ django_ecom/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for vanna_club project.
+WSGI config for django_ecom project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vanna_club.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_ecom.settings')
 
 application = get_wsgi_application()
